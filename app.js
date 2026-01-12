2387b50def7cac567af7bddf763800aeeda5db43
G3 <34997807+GlueGunGlitter@users.noreply.github.com>
2026-01-12 18:26:01 +0200

    did autonomus

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
-let score = 0;
-
+// Auto Score Counter
+let score1 = 0;
 const scoreSpan = document.getElementById("score");
 const addPointBtn = document.getElementById("addPointBtn");
-const removePointBtn =  document.getElementById("removePointBtn");
+const removePointBtn = document.getElementById("removePointBtn");
 
 addPointBtn.addEventListener("click", () => {
-    score++;
-    scoreSpan.textContent = score;
-    
+    score1++;
+    scoreSpan.textContent = score1;
 });
+
 removePointBtn.addEventListener("click", () => {
-    if(score <= 0){
-      
-        return
-    }else{    score--;
-}
-    
-    scoreSpan.textContent = score;
-    
+    if(score1 > 0) score1--;
+    scoreSpan.textContent = score1;
 });
+
+// Times Missed Counter
+let misses1 = 0;
+const missSpan = document.getElementById("missCount");
+const addMissBtn = document.getElementById("addMissBtn");
+const removeMissBtn = document.getElementById("removeMissBtn");
+
+addMissBtn.addEventListener("click", () => {
+    misses1++;
+    missSpan.textContent = misses1;
+});
+
+removeMissBtn.addEventListener("click", () => {
+    if(misses1 > 0) misses1--;
+    missSpan.textContent = misses1;
+});
+
